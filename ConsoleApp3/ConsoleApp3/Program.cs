53743706cf676367de537a077d279ced729fba3e
didosimi324 <d.rusev2017@pmg-pernik.com>
2021-09-29 09:30:56 +0300

    add project

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+
+            Console.WriteLine(string.Join(" ", nums.Reverse()));
+        }
+    }
+}
